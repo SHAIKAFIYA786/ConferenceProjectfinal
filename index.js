20f2de3ee9a61f6ab5de81df1bfbe20de342c778
Shaik Afiya <shaikafiya9676@gmail.com>
2025-04-02 17:15:26 +0530

    added all color and  maked it responsive

--- index.js
+++ index.js
@@ -351,7 +351,7 @@ app.get("/papers/:id", async (req, res) => {
 });
 
 // 🔹 Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
